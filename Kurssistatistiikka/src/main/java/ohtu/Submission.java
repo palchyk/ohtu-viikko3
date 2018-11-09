5fe8a22922f298bf188a4b6a08641a3435895e5d
Stas <palchyk@github.com>
2018-11-09 12:27:57 +0200

    tehtävä1

--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,19 +1,51 @@
 package ohtu;
 
+import java.util.ArrayList;
+
 public class Submission {
     private int week;
+     private int hours;
+     private ArrayList<Integer> exercises;
+     private String course;
 
     public void setWeek(int week) {
         this.week = week;
     }
 
+    public ArrayList<Integer> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(ArrayList<Integer> exercises) {
+        this.exercises = exercises;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
     public int getWeek() {
         return week;
     }
+    
+     public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+    
+    
 
     @Override
     public String toString() {
-        return ""+week;
+        String teht =   exercises.toString().substring(1, exercises.toString().length()-1);
+        return course+", viikko "+week+" tehtyjä tehtäviä yhteensä "+exercises.size()+" aikaa kului "+hours+" tehdyt tehtävät: "+teht+" ";
     }
     
 }
